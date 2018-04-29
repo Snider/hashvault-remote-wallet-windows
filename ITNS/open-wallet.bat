f3983ea31920f80544fad7199fcc8fde6f6c6250
Snider <snider@hashvault.pro>
2018-04-29 21:47:53 +0100

    updates itns wallet to v2.0.1

--- ITNS/open-wallet.bat
+++ ITNS/open-wallet.bat
@@ -1,2 +1,2 @@
 cd data
-..\intensecoin-cli-win64-v200\intense-wallet-cli.exe --daemon-host=nodes.hashvault.pro --wallet-file=wallet
+..\intensecoin-cli-win64-v201\intense-wallet-cli.exe --daemon-host=nodes.hashvault.pro --wallet-file=wallet
--- ITNS/restore-from-seed.bat
+++ ITNS/restore-from-seed.bat
@@ -1,2 +1,2 @@
 cd data
-..\intensecoin-cli-win64-v200\intense-wallet-cli.exe --restore-deterministic-wallet --daemon-host=nodes.hashvault.pro --generate-new-wallet=wallet
+..\intensecoin-cli-win64-v201\intense-wallet-cli.exe --restore-deterministic-wallet --daemon-host=nodes.hashvault.pro --generate-new-wallet=wallet
