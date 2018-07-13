902496f9ba185e5629505fb16ca5b3afc9744273
Snider <snider@hashvault.pro>
2018-07-13 10:58:13 +0100

    adds ryo

---
+++ RYO/restore-from-seed.bat
@@ -0,0 +1,2 @@
+cd data
+..\ryo-win-x64-0.2.0.2\ryo-wallet-cli.exe --restore-deterministic-wallet --daemon-host=nodes.hashvault.pro --generate-new-wallet=wallet
---
+++ RYO/start-new-wallet.bat
@@ -0,0 +1,2 @@
+cd data
+..\ryo-win-x64-0.2.0.2\ryo-wallet-cli.exe --daemon-host=nodes.hashvault.pro --generate-new-wallet=wallet
