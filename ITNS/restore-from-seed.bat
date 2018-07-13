6e414ecaeeceee61a5fba1b7e6dce2943c8a726b
Snider <snider@hashvault.pro>
2018-07-13 11:16:58 +0100

    updates ITNS

--- ITNS/restore-from-seed.bat
+++ ITNS/restore-from-seed.bat
@@ -1,2 +1,2 @@
 cd data
-..\intensecoin-cli-win64-v201\intense-wallet-cli.exe --restore-deterministic-wallet --daemon-host=nodes.hashvault.pro --generate-new-wallet=wallet
+..\intensecoin-cli-win-64bit-v2.0.2.1.1\intensecoin-cli-win-64bit-v2.0.2.1\intense-wallet-cli.exe --restore-deterministic-wallet --daemon-host=nodes.hashvault.pro --generate-new-wallet=wallet
